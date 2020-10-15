b4f11b24452daa467e8a8fcc7d01ccbc4cb141d6
xrudoga <defcw58@gmail.com>
2020-10-15 16:49:25 +0800

    Implementing the methods in the Generic repository

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,41 +7,40 @@ using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
-{
+namespace API.Controllers {
     [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
-    {
-        private readonly IProductRepository _repo;
-        public ProductsController(IProductRepository repo)
+    [Route ("api/[controller]")]
+    public class ProductsController : ControllerBase {
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+        public ProductsController (IGenericRepository<Product> productsRepo, IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo) 
         {
-            _repo = repo;
-
+            _productTypeRepo = productTypeRepo;
+            _productBrandRepo = productBrandRepo;
+            _productsRepo = productsRepo;
         }
+
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
-        {
-            var products = await _repo.GetProductsAsync();
+        public async Task<ActionResult<List<Product>>> GetProducts () {
+            var products = await _productsRepo.ListAllAsync();
 
-            return Ok(products);
-        }
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id)
-        {
-            return await _repo.GetProductByIdAsync(id);
+            return Ok (products);
         }
 
-        [HttpGet("brands")]
-        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands(int id)
-        {
-            return Ok(await _repo.GetProductBrandsAsync());
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Product>> GetProduct (int id) {
+            return await _productsRepo.GetByIdAsync(id);
         }
 
-        [HttpGet("types")]
-        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes(int id)
-        {
-            return Ok(await _repo.GetProductTypesAsync());
+        [HttpGet ("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands (int id) {
+            return Ok (await _productBrandRepo.ListAllAsync());
+        }
+
+        [HttpGet ("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes (int id) {
+            return Ok (await _productTypeRepo.ListAllAsync());
         }
     }
 }
--- Infrastructure/GenericRepository.cs
+++ Infrastructure/GenericRepository.cs
@@ -2,19 +2,22 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interface;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure
-{
-    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
-    {
-        public Task<T> GetByIdAsync(int id)
-        {
-            throw new System.NotImplementedException();
+namespace Infrastructure {
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity {
+        private readonly StoreContext _context;
+        public GenericRepository (StoreContext context) {
+            _context = context;
         }
 
-        public Task<IReadOnlyList<T>> ListAllAsync()
-        {
-            throw new System.NotImplementedException();
+        public async Task<T> GetByIdAsync (int id) {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync () {
+            return await _context.Set<T>().ToListAsync();
         }
     }
 }
