4091e82fbc6a3db17dec0770eacdccd2ea1c5235
xrudoga <defcw58@gmail.com>
2020-10-24 16:45:58 +0800

    Test token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using API.Dto;
 using API.Errors;
 using Core.Entities.Identity;
+using Core.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,49 +11,45 @@ namespace API.Controllers {
     public class AccountController : BaseApiController {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        public AccountController (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager) 
-        {
+        private readonly ITokenService _tokenService;
+        public AccountController (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService) {
+            _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-        {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+        [HttpPost ("login")]
+        public async Task<ActionResult<UserDto>> Login (LoginDto loginDto) {
+            var user = await _userManager.FindByEmailAsync (loginDto.Email);
 
-            if (user == null) return Unauthorized(new ApiResponse(401));
+            if (user == null) return Unauthorized (new ApiResponse (401));
 
-            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            var result = await _signInManager.CheckPasswordSignInAsync (user, loginDto.Password, false);
 
-            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+            if (!result.Succeeded) return Unauthorized (new ApiResponse (401));
 
-            return new UserDto
-            {
+            return new UserDto {
                 Email = user.Email,
-                Token = "This will be a token",
-                DisplayName = user.DisplayName
+                    Token = _tokenService.CreateToken(user),
+                    DisplayName = user.DisplayName
             };
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-        {
-            var user = new AppUser
-            {
+        [HttpPost ("register")]
+        public async Task<ActionResult<UserDto>> Register (RegisterDto registerDto) {
+            var user = new AppUser {
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
                 UserName = registerDto.Email
             };
-            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            var result = await _userManager.CreateAsync (user, registerDto.Password);
 
-            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+            if (!result.Succeeded) return BadRequest (new ApiResponse (400));
 
-            return new UserDto
-            {
+            return new UserDto {
                 DisplayName = user.DisplayName,
-                Token = " This will be a token",
-                Email = user.Email
+                    Token = _tokenService.CreateToken(user),
+                    Email = user.Email
             };
         }
     }
