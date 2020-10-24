4e6fef08b4609904af31fdeb8cf8ec88d81d07dd
xrudoga <defcw58@gmail.com>
2020-10-24 16:28:24 +0800

    Setting up identity to use the token

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,7 +38,7 @@ namespace API
                 return ConnectionMultiplexer.Connect(configuration);
             });
             services.AddApplicationServices();
-            services.AddIdentityServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
             services.AddCors(opts => 
             {
@@ -63,6 +63,7 @@ namespace API
 
             app.UseCors("CorsPolicy");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseSwaggerDocument();
