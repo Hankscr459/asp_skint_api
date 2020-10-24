d68c4a473c7d3fae9a1ee6c8416623fe430d5f42
xrudoga <defcw58@gmail.com>
2020-10-24 22:52:56 +0800

    Adding another Dto for the user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Dto;
 using API.Errors;
 using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interface;
 using Microsoft.AspNetCore.Authorization;
@@ -15,7 +16,9 @@ namespace API.Controllers {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
-        public AccountController (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService) {
+        private readonly IMapper _mapper;
+        public AccountController (UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper) {
+            _mapper = mapper;
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
@@ -23,32 +26,42 @@ namespace API.Controllers {
 
         [Authorize]
         [HttpGet]
-        public async Task<ActionResult<UserDto>> GetCurrentUser()
-        {
+        public async Task<ActionResult<UserDto>> GetCurrentUser () {
 
-            var user = await _userManager.FindByEmailFromClaimsPrinciple(HttpContext.User);
+            var user = await _userManager.FindByEmailFromClaimsPrinciple (HttpContext.User);
 
-            return new UserDto
-            {
+            return new UserDto {
                 Email = user.Email,
-                Token = _tokenService.CreateToken(user),
-                DisplayName = user.DisplayName
+                    Token = _tokenService.CreateToken (user),
+                    DisplayName = user.DisplayName
             };
         }
 
-        [HttpGet("emailexists")]
-        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
-        {
-            return await _userManager.FindByEmailAsync(email) != null;
+        [HttpGet ("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync ([FromQuery] string email) {
+            return await _userManager.FindByEmailAsync (email) != null;
         }
 
         [Authorize]
-        [HttpGet("address")]
-        public async Task<ActionResult<Address>> GetUserAddress()
+        [HttpGet ("address")]
+        public async Task<ActionResult<AddressDto>> GetUserAddress () {
+            var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
+
+            return _mapper.Map<Address, AddressDto>(user.Address);
+        }
+        [Authorize]
+        [HttpPut("address")]
+        public async Task<ActionResult<AddressDto>> UpdateUserAddress(AddressDto address)
         {
             var user = await _userManager.FindByUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
 
-            return user.Address;
+            user.Address = _mapper.Map<AddressDto, Address>(address);
+
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+
+            return BadRequest("Problem updating the user");
         }
 
         [HttpPost ("login")]
@@ -57,13 +70,13 @@ namespace API.Controllers {
 
             if (user == null) return Unauthorized (new ApiResponse (401));
 
-            var result = await _signInManager.CheckPasswordSignInAsync (user, loginDto.Password, false);
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
             if (!result.Succeeded) return Unauthorized (new ApiResponse (401));
 
             return new UserDto {
                 Email = user.Email,
-                    Token = _tokenService.CreateToken(user),
+                    Token = _tokenService.CreateToken (user),
                     DisplayName = user.DisplayName
             };
         }
@@ -81,7 +94,7 @@ namespace API.Controllers {
 
             return new UserDto {
                 DisplayName = user.DisplayName,
-                    Token = _tokenService.CreateToken(user),
+                    Token = _tokenService.CreateToken (user),
                     Email = user.Email
             };
         }
