cf3948edfac07f4df1815e1a81cb7c59c3b65941
xrudoga <defcw58@gmail.com>
2020-10-24 19:52:55 +0800

    Troubleshooting auth issues

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -31,6 +31,7 @@ namespace Infrastructure.Services {
             {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
                 Issuer = _config["Token:Issuer"]
             };
             var tokenHandler = new JwtSecurityTokenHandler();
